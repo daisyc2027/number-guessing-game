1c0be89c35428b81c1bfddb80ad558e70e3cf2f1
daisychen <daisyc2027@student.cis.edu.hk>
2024-09-08 14:00:18 +0800

    guessing the number except its in a function

--- src/Main.java
+++ src/Main.java
@@ -2,43 +2,45 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
 
         int maxRange = 500;
         int minRange = 100;
-        int randomNumber = (int) (Math.random() * ((maxRange+1) - minRange) + minRange);
+        int randomNumber = (int) (Math.random() * ((maxRange + 1) - minRange) + minRange);
         System.out.println("you have 5 tries to guess the number i'm thinking of between " + minRange + " and " + maxRange);
         System.out.println("For reference purposes, the number is: " + randomNumber);
 
+        playGame(randomNumber, maxRange, minRange);
+    }
 
+
+    public static void playGame(int randomNumber, int maxRange, int minRange) {
+        Scanner sc = new Scanner(System.in);
         int userGuess;
         System.out.println("guess a number between " + minRange + " and " + maxRange);
         userGuess = sc.nextInt();
         int numberGuesses = 4;
 
-        for(int i = numberGuesses; i > 0; i--) {
+        for (int i = numberGuesses; i > 0; i--) {
             if (userGuess > maxRange || userGuess < minRange) {
                 System.out.println("pls read the question again, " + i + " tries left");
                 userGuess = sc.nextInt();
-            }
-            else if (userGuess < randomNumber){
+            } else if (userGuess < randomNumber) {
                 System.out.println("too small, " + i + " tries left");
                 userGuess = sc.nextInt();
-            }
-            else if (userGuess > randomNumber) {
+            } else if (userGuess > randomNumber) {
                 System.out.println("too big " + i + " tries left");
                 userGuess = sc.nextInt();
-            }
-            else {
+            } else {
                 break;
             }
         }
-        if(randomNumber != userGuess) {
+        if (randomNumber != userGuess) {
             System.out.println("lol you lose, the number was " + randomNumber);
-        }
-        else {
+        } else {
             System.out.println("yeah u win :)");
         }
+    }
+}
 
 //        while(numberGuesses >= 0){
 //    if (userGuess <= 100 && userGuess >= 0) {
@@ -63,5 +65,4 @@ public class Main {
 //    if(numberGuesses == 0){
 //        System.out.println("Sorry, the correct answer was: " + randomNumber + ". Better luck next time!");
 //    }
-    }
-}
+
